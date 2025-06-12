d510fb3a0e13aca1a265f505dfdd10b97066340d
Bruise Whine <francisca.fernandez2301@alumnos.ubiobio.cl>
2025-06-11 22:21:39 -0400

    Update HerramientaController.java

--- Controller/HerramientaController.java
+++ Controller/HerramientaController.java
@@ -1,2 +1,30 @@
+package Controller;
+
+import DAO.HerramientaDAO;
+import Model.Herramienta;
+import java.util.List;
+
+
 public class HerramientaController {
+    private HerramientaDAO dao;
+
+    public HerramientaController() {
+        this.dao = new HerramientaDAO();
+    }
+
+    public void crearHerramienta(String nombre, String tipo, String estado) {
+        dao.insertar(new Herramienta(nombre, tipo, estado));
+    }
+    public List<Herramienta> obtenerHerramientas() {
+        return dao.listar();
+    }
+    public Herramienta obtenerHerramientaPorId(int id) {
+        return dao.buscarPorId(id);
+    }
+    public void actualizarHerramienta(int id, String nombre, String tipo, String estado) {
+        dao.actualizar(new Herramienta(id, nombre, tipo, estado));
+    }
+    public void eliminarHerramienta(int id) {
+        dao.eliminar(id);
+    }
 }
