a486baf8e7a8d7a5fb7debbe4dd6b613bb8e0b98
Bruise Whine <francisca.fernandez2301@alumnos.ubiobio.cl>
2025-06-11 22:20:54 -0400

    Update ClienteController.java

--- Controller/ClienteController.java
+++ Controller/ClienteController.java
@@ -1,2 +1,29 @@
+package Controller;
+
+import DAO.ClienteDAO;
+import Model.Cliente;
+import java.util.List;
+
 public class ClienteController {
+    private ClienteDAO dao;
+
+    public ClienteController() {
+        this.dao = new ClienteDAO();
+    }
+
+    public void crearCliente(String nombre, String rut, String telefono) {
+        dao.insertar(new Cliente(nombre, rut, telefono));
+    }
+    public List<Cliente> obtenerClientes() {
+        return dao.listar();
+    }
+    public Cliente obtenerClientePorId(int id) {
+        return dao.buscarPorId(id);
+    }
+    public void actualizarCliente(int id, String nombre, String rut, String telefono) {
+        dao.actualizar(new Cliente(id, nombre, rut, telefono));
+    }
+    public void eliminarCliente(int id) {
+        dao.eliminar(id);
+    }
 }
