ef64aa79656e939c7800a80b9a65e54cf273ec76
Bruise Whine <francisca.fernandez2301@alumnos.ubiobio.cl>
2025-06-14 18:07:20 -0400

    Update Conexion.java

--- Model/Conexion.java
+++ Model/Conexion.java
@@ -1,1 +1,15 @@
+package Model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    private static final String URL = "jdbc:h2:~/tallerdb";
+    private static final String USUARIO = "sa";
+    private static final String PASAR = "";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USUARIO, PASAR);
+    }
+}
