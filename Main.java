a45cabe9fa6cda51dc75bc40ce3caee9c6cc290a
Jav <javiera.duarte2201@alumnos.ubiobio.cl>
2025-06-14 16:37:30 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,39 @@
-import View.MenuPrincipal;
+import Controller.*;
+import Util.Validador;
 
 public class Main {
-    public static void main(String[] args) {
-        MenuPrincipal menu = new MenuPrincipal();
-        menu.mostrar();
-        System.out.println("Finalizando... ");
-    }
+    public static void main(String[] args) { // Inicio
+
+         AutoController auto = new AutoController();
+         ClienteController cliente = new ClienteController();
+         MecanicoController mecanico = new MecanicoController();
+         HerramientaController herramienta = new HerramientaController();
+         Validador validador = new Validador();
+        int opcion;
+
+        do {
+            System.out.println("Bienvenido al Menu Principal");
+            System.out.println("1.- Ingresar al menu Autos");
+            System.out.println("2.- Ingresar al menu Clientes");
+            System.out.println("3.- Ingresar al menu Herramientas");
+            System.out.println("4.- Ingresar al menu Mecanicos");
+            System.out.println("5.- Salir");
+
+            opcion = validador.validarInt(1);
+
+            switch (opcion){
+
+                case 1 -> auto.iniciar();
+                case 2 -> cliente.iniciar();
+                case 3 -> herramienta.iniciar();
+                case 4 -> mecanico.iniciar();
+                case 5 -> System.out.println("Bye Bye");
+
+                default -> System.out.println("Ingrese un numero valido" + "\n");
+
+            }
+
+        } while (opcion != 5);
+
+    } // Final
 }
