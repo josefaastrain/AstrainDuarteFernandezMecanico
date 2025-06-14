ed26460fce75c5bebe4c2ecccc4fa1f26dda5bd4
Jav <javiera.duarte2201@alumnos.ubiobio.cl>
2025-06-14 10:13:29 -0400

    Update Auto.java

--- Model/Auto.java
+++ Model/Auto.java
@@ -1,25 +1,24 @@
 package Model;
 
-
 public class Auto {
-  private String id_auto;
-  private String marca;
-  private String modelo;
-  private int anio;
+  private int id_auto;
+		  private String marca;
+		  private String modelo;
+		  private int anio;
   private String patente;
-  
-  public Auto(String id_auto, String marca, String modelo, int anio, String patente){
-    this.id_auto=id_auto;
-    this.marca=marca;
-    this.modelo=modelo;
-    this.anio=anio;
-    this.patente=patente;
+
+  public Auto(int id_Auto, String marca, String modelo, int anio, String patente) {
+    this.marca = marca;
+    this.id_auto = id_Auto;
+    this.modelo = modelo;
+    this.anio = anio;
+    this.patente = patente;
   }
   
   public Auto(){
   }
   
-  public String getId_auto(){
+  public int getId_auto(){
     return id_auto;
   }
   public String getMarca(){
@@ -34,7 +33,7 @@ public class Auto {
   public String getPatente(){
     return patente;
   }
-  public void setId_auto(String id_auto) {
+  public void setId_auto(int id_auto) {
         this.id_auto = id_auto;
   }
   public void setMarca(String marca) {
@@ -49,6 +48,8 @@ public class Auto {
   public void setPatente(String patente) {
         this.patente = patente;
   }
+}
+
   @Override
   public String toString() {
         return String.format("%d | %s | %s | %d | %s", idAuto, marca, modelo, anio, patente);
